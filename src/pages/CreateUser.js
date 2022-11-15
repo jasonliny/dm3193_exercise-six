76eece933268e11e4679507ad2d489bce3042391
Jason <70929180+jasonliny@users.noreply.github.com>
2022-11-15 09:21:23 -0500

    Setup pages, components, and routes

---
+++ src/pages/CreateUser.js
@@ -0,0 +1,12 @@
+import React from "react";
+import CreateUserForm from "../components/CreateUserForm";
+
+function CreateUserPage() {
+  return (
+    <div>
+      <h1>Create User</h1>
+      <CreateUserForm />
+    </div>
+  );
+}
+export default CreateUserPage;
